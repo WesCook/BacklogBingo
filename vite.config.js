4473575f968619d34fb62c07d0ad274b5f06a51c
Wes Cook <im@wescook.ca>
2024-03-27 09:40:19 -0700

    Add linting rules

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 const deployPath = '/BacklogBingo'; // GitHub repo folder as base path
+// eslint-disable-next-line no-undef
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
 	plugins: [ vue() ],
 	base: (isProduction) ? deployPath : '/',
-	outDir: 'dist'
-})
+	outDir: 'dist',
+	define: {
+		// Remove Options API during build for ~3KB smaller bundles
+		// https://vuejs.org/api/compile-time-flags#VUE_OPTIONS_API
+		__VUE_OPTIONS_API__: false
+	}
+});
