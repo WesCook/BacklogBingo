a5245e99cb7bf4b2125b82da3ebf22cd62b2b50c
Wes Cook <im@wescook.ca>
2024-04-16 18:23:35 -0700

    Setting per-page titles

--- src/router.js
+++ src/router.js
@@ -15,48 +15,87 @@ import BingoCard from './pages/BingoCard.vue';
 import NotFound from './pages/NotFound.vue';
 
 const routes = [
-	{ path: '/', component: NotFound, beforeEnter: () => {
-		if (isBingoCardSet.value) {
-			return '/bingo';
-		} else if (isCardSourceSet.value) {
-			return '/gamerules';
-		} else {
-			return '/card';
-		}
-	} },
-	{ path: '/card', component: CardSource, beforeEnter: () => {
-		if (isBingoCardSet.value) {
-			setError('You cannot access that page once a bingo card has been generated.  You have been redirected.');
-			return '/bingo';
-		}
-	}},
-	{ path: '/gamerules', component: GameRules, beforeEnter: () => {
-		if (!isCardSourceSet.value && !isBingoCardSet.value) {
-			setError('You cannot access that page without first defining a card source or bingo card.  You have been redirected to do so now.');
-			return '/card';
-		}
-	}},
-	{ path: '/categories', component: RefineCategories, beforeEnter: () => {
-		if (!isCardSourceSet.value) {
-			setError('You cannot access that page without first defining a card source.  You have been redirected to do so now.');
-			return '/card';
-		}
-		if (isBingoCardSet.value) {
-			setError('You cannot access that page once a bingo card has been generated.  You have been redirected.');
-			return '/bingo';
-		}
-	}},
-	{ path: '/bingo', component: BingoCard, beforeEnter: () => {
-		if (!isBingoCardSet.value) {
-			setError('You cannot access that page without first generating a bingo card.  You have been redirected to do so now.');
-			if (!isCardSourceSet.value) {
-				return '/card';
-			} else {
+	{
+		// Just using the index as a redirect
+		path: '/',
+		component: NotFound,
+		beforeEnter: () => {
+			if (isBingoCardSet.value) {
+				return '/bingo';
+			} else if (isCardSourceSet.value) {
 				return '/gamerules';
+			} else {
+				return '/card';
 			}
 		}
-	}},
-	{ path: '/:pathMatch(.*)*', component: NotFound }
+	},
+	{
+		path: '/card',
+		component: CardSource,
+		meta: {
+			title: 'Card Source - Backlog Bingo'
+		},
+		beforeEnter: () => {
+			if (isBingoCardSet.value) {
+				setError('You cannot access that page once a bingo card has been generated.  You have been redirected.');
+				return '/bingo';
+			}
+		}
+	},
+	{
+		path: '/gamerules',
+		component: GameRules,
+		meta: {
+			title: 'Game Rules - Backlog Bingo'
+		},
+		beforeEnter: () => {
+			if (!isCardSourceSet.value && !isBingoCardSet.value) {
+				setError('You cannot access that page without first defining a card source or bingo card.  You have been redirected to do so now.');
+				return '/card';
+			}
+		}
+	},
+	{
+		path: '/categories',
+		component: RefineCategories,
+		meta: {
+			title: 'Refine Categories - Backlog Bingo'
+		},
+		beforeEnter: () => {
+			if (!isCardSourceSet.value) {
+				setError('You cannot access that page without first defining a card source.  You have been redirected to do so now.');
+				return '/card';
+			}
+			if (isBingoCardSet.value) {
+				setError('You cannot access that page once a bingo card has been generated.  You have been redirected.');
+				return '/bingo';
+			}
+		}
+	},
+	{
+		path: '/bingo',
+		component: BingoCard,
+		meta: {
+			title: 'Backlog Bingo'
+		},
+		beforeEnter: () => {
+			if (!isBingoCardSet.value) {
+				setError('You cannot access that page without first generating a bingo card.  You have been redirected to do so now.');
+				if (!isCardSourceSet.value) {
+					return '/card';
+				} else {
+					return '/gamerules';
+				}
+			}
+		}
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		component: NotFound,
+		meta: {
+			title: '404 - Backlog Bingo'
+		}
+	}
 ];
 
 const router = createRouter({
@@ -72,8 +111,11 @@ const router = createRouter({
 	},
 });
 
-// Clear errors when changing page (though skip redirects so redirect notices persist)
 router.beforeEach(route => {
+	// Update title
+	document.title = route.meta.title;
+
+	// Clear errors when changing page (though skip redirects so redirect notices persist)
 	if (!route.redirectedFrom) {
 		clearError();
 	}
